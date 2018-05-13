fa697f9333db2a20e266f2add3d3aa6e2aa11c3e
Ignas Asmontas <ignas.asmontas@gmail.com>
2018-05-13 11:24:07 +0300

    Initial hosted db commit

--- swagger_server/controllers/data_controller.py
+++ swagger_server/controllers/data_controller.py
@@ -1,5 +1,7 @@
 import connexion
 import six
+import os
+import MySQLdb
 
 from swagger_server.models.data import Data  # noqa: E501
 from swagger_server import util
@@ -18,3 +20,21 @@ def add_data(body=None):  # noqa: E501
     if connexion.request.is_json:
         body = Data.from_dict(connexion.request.get_json())  # noqa: E501
         return body
+
+    user = os.environ["MYSQL_USER"]
+    passwd = os.environ["MYSQL_PASSWORD"]
+    dbhost = os.environ["MYSQL_SERVICE_HOST"]
+    dbname = os.environ["MYSQL_DATABASE"]
+
+    db = MySQLdb.connect(host=dbhost,
+                        user=user,
+                        passwd=passwd,
+                        db=dbname)
+
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM `sensor_data`")
+    
+    for row in cur.fetchall():
+        print("{} {} {}".format(row[0], row[1], row[2]))
