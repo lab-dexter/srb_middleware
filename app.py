17870df5fc17f7698265b313f92c131513e9e8d1
Ignas Asmontas <ignas.asmontas@gmail.com>
2018-05-26 08:24:20 +0300

    Added insert into the DB. DB object is created on each API call. Further refactoring will be needed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from swagger_server import encoder
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger_server/swagger/')
+    app = connexion.App(__name__, specification_dir='./swagger_server/swagger/', debug=True,)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Smart recycling bins'})
     app.run(port=8080)
--- swagger_server/controllers/data_controller.py
+++ swagger_server/controllers/data_controller.py
@@ -1,5 +1,8 @@
 import connexion
 import six
+import os
+import MySQLdb
+import logging
 
 from swagger_server.models.data import Data  # noqa: E501
 from swagger_server import util
@@ -15,6 +18,30 @@ def add_data(body=None):  # noqa: E501
 
     :rtype: Data
     """
+
     if connexion.request.is_json:
         body = Data.from_dict(connexion.request.get_json())  # noqa: E501
+
+        user = os.environ["MYSQL_USER"]
+        passwd = os.environ["MYSQL_PASSWORD"]
+        dbhost = os.environ["MYSQL_SERVICE_HOST"]
+        dbname = os.environ["MYSQL_DATABASE"]
+
+        # TODO: Refactor DB object initialization elsewhere, e.g. during app start
+        db = MySQLdb.connect(host=dbhost,
+                            user=user,
+                            passwd=passwd,
+                            db=dbname)        
+
+        cur = db.cursor()
+
+        mac_id = "{}_{}".format(body.mac, body.id)
+        print("Data to be inserted: {}; {}; {}".format(mac_id, body.data, body.time))
+        try:
+            cur.execute("""INSERT INTO `sensor_data` (`macid`, `distance`, `timestamp`) VALUES (%s, %s, %s)""",(mac_id, body.data, body.time))
+            db.commit()
+        except Exception as e:
+            print("Failed to insert data: {}. Rollbacking db commit...".format(e))
+            db.rollback()
+            
         return body
